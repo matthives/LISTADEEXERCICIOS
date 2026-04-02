d6d882736ea6425493b7c0accaa19e4af7f00ea2
Mateus Thives <mateus.thives@hotmail.com>
2026-04-02 20:53:18 -0300

    Finalizado projeto cálculo média notas

---
+++ Exercicio05.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// Crie um programa para calcular a média ponderada de duas provas realizadas por um aluno
+
+Console.Write("Digite a nota da primeira prova: ");
+double primeiraNota = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite a nota da segunda prova: ");
+double segundaNota = Convert.ToDouble(Console.ReadLine());
+
+double media = (primeiraNota + segundaNota) / 2;
+
+Console.WriteLine($"A média das duas provas é: {media:F2}");
+
+Console.ReadLine();
