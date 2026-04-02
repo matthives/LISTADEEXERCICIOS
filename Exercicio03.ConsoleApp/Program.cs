28cbf3ab7bf572a3e292eaf6dde319e4dda95d98
Mateus Thives <mateus.thives@hotmail.com>
2026-04-02 20:29:07 -0300

    Finalizado projeto converter a temperatura Celsius para escala Fahrenheit

---
+++ Exercicio03.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+
+
+Console.Write("Digite a temperatua em celsius: ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+
+double fahrenheit = (celsius * 1.8) + 32;
+
+Console.WriteLine($"A escala em fahrenheit é: {celsius}ºc equivale a {fahrenheit:F2} ºF");
+
+Console.ReadLine();
