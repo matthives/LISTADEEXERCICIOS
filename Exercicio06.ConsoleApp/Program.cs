3d67ec7afb6e4d62f1d1bdb0d7f6b192792957e6
Mateus Thives <mateus.thives@hotmail.com>
2026-04-02 21:03:53 -0300

    Finalizado projeto de leitura de valores

---
+++ Exercicio06.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor
+// que C.
+
+Console.Write("Digite o valor de A: ");
+double valorA = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o valor de B: ");
+double valorB = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o valor de C: ");
+double valorC = Convert.ToDouble(Console.ReadLine());
+
+if (valorA + valorB < valorC)
+{
+    Console.WriteLine($"\nA soma de A + B ({valorA + valorB}) é MENOR que C ({valorC}).");
+}
+
+Console.ReadLine();
