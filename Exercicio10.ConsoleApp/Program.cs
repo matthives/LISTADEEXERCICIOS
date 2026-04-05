6156243298711e857ef2364587fceaa5100a1985
Mateus Thives <mateus.thives@hotmail.com>
2026-04-05 20:27:22 -0300

    Finalizado projeto FizzBuzz

---
+++ Exercicio10.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// Escreva um programa que imprima os números de 1 a 100 em ordem crescente, substituindo
+// os números múltiplos de 3 pela palavra "Fizz" e os múltiplos de 5 pela palavra "Buzz". Para
+// números que são múltiplos de ambos, use "FizzBuzz".
+
+
+for (int numero = 1; numero <= 100; numero++)
+{
+    if (numero % 3 == 0 && numero % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (numero % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (numero % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(numero);
+    }
+}
